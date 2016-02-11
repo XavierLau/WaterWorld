c19d59b5c2c2ae8fa1558d91223a694d730543ab
Connor Phalen <Phalen.Connor@hotmail.co.uk>
2016-02-10 22:21:25 -0800

    Added more countries and intents.

---
+++ app/src/main/java/project/comp3717/bcit/ca/waterworld/AsiaActivity.java
@@ -0,0 +1,19 @@
+package project.comp3717.bcit.ca.waterworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class AsiaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asia);
+    }
+
+    public void viewCountryInfo(final View view){
+        Toast.makeText(this, "Text", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/project/comp3717/bcit/ca/waterworld/WorldActivity.java
@@ -0,0 +1,47 @@
+package project.comp3717.bcit.ca.waterworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WorldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_world);
+    }
+
+    public void changeActivity(final View view)
+    {
+        Intent intent;
+        switch((String)view.getTag()){
+            case "0":
+                intent = new Intent(this, AsiaActivity.class);
+                break;
+            case "1":
+                intent = new Intent(this, AfricaActivity.class);
+                break;
+            case "2":
+                intent = new Intent(this, EuropeActivity.class);
+                break;
+            case "3":
+                intent = new Intent(this, NorthAmericaActivity.class);
+                break;
+            case "4":
+                intent = new Intent(this, AntarcticaActivity.class);
+                break;
+            case "5":
+                intent = new Intent(this, OceaniaActivity.class);
+                break;
+            case "6":
+                intent = new Intent(this, SouthAmericaActivity.class);
+                break;
+            default:
+                intent = new Intent(this, WorldActivity.class);
+                break;
+        }
+        startActivity(intent);
+    }
+}
