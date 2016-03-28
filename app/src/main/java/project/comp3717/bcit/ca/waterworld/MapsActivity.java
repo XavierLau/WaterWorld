cc2278be92bb66cb626d941a99a3697c75f88ed6
XavierLau <xavierlau1@gmail.com>
2016-03-28 11:30:40 -0700

    added polygons for all the countries

--- app/src/main/java/project/comp3717/bcit/ca/waterworld/MapsActivity.java
+++ app/src/main/java/project/comp3717/bcit/ca/waterworld/MapsActivity.java
@@ -4,25 +4,40 @@ import android.content.ContentValues;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import project.comp3717.bcit.ca.waterworld.ContinentContract.ContinentEntry;
-import project.comp3717.bcit.ca.waterworld.CountryContract.CountryEntry;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.maps.CameraUpdateFactory;
+import android.util.Log;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.geojson.GeoJsonLayer;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 {
     private DataDBHelper                dataDBHelper;
     private ArrayList<String> continentNames;
-    private GoogleMap mMap;
+    private GoogleMap googleMap;
+    private final String ftCountryAPIKey = "AIzaSyAm3E9BguE5FWR8IGPaj3YT0XowwRYa4Rk";
+    private final String ftCountryDataURL = "https://www.googleapis.com/fusiontables/v2/query?sql="
+                                            + "SELECT%20*%20FROM%201Olm63OBLeyofjNSThCCLnxRgXJV0g3"
+                                            + "0iQK-J5tKu&key=";
+    private final ArrayList<GeoJsonLayer> geoJsonLayerArrayList = new ArrayList<GeoJsonLayer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -49,7 +64,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         res                             = getResources();
         //countries                       = res.getStringArray(R.array.countries);         //change this when we get countries
         //continents                      = res.getStringArray(R.array.continents);        //change this when we get continents
-        continentNames                  = new ArrayList<String>(7);
+        continentNames                  = new ArrayList<String>();
 
         /*
         // inserting data into country table
@@ -97,6 +112,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         cursor.close();
         */
+
     }
 
 
@@ -112,6 +128,125 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap)
     {
-        mMap = googleMap;
+        final RetrieveCountryData retrieveCountryData;
+
+        this.googleMap = googleMap;
+        retrieveCountryData = new RetrieveCountryData();
+        retrieveCountryData.execute(ftCountryDataURL + ftCountryAPIKey);
+
+        /*InputStream stream = getResources().openRawResource(R.raw.test);
+
+        String line;
+        StringBuilder result = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+        try
+        {
+            while ((line = reader.readLine()) != null) {
+                // Read and save each line of the stream
+                result.append(line);
+            }
+            // Close the stream
+            reader.close();
+            stream.close();
+            JSONObject jsonObject = new JSONObject(result.toString());
+            JSONArray rows;
+            rows = jsonObject.getJSONArray("rows");
+            JSONArray countries;
+            countries = rows.getJSONArray(0);
+
+            Log.d("json", countries.getString(1));
+            geoJsonLayer = new GeoJsonLayer(this.googleMap, new JSONObject(countries.getString(1)));
+            geoJsonLayer.addLayerToMap();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }*/
+
+        /*try {
+            geoJsonLayer = new GeoJsonLayer(googleMap, R.raw.test, getApplicationContext());
+            geoJsonLayer.addLayerToMap();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e)
+        {
+            e.printStackTrace();
+        }*/
+    }
+
+    private class RetrieveCountryData extends AsyncTask<String, Void, JSONObject>
+    {
+        @Override
+        protected JSONObject doInBackground(String... params)
+        {
+            final URL url;
+            final HttpURLConnection httpURLConnection;
+            final JSONObject jsonObject;
+            final InputStream inputStream;
+            String line;
+            StringBuilder result = new StringBuilder();
+            BufferedReader bufferedReader;
+
+            try
+            {
+                url = new URL(params[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                inputStream = new BufferedInputStream(httpURLConnection.getInputStream());
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    result.append(line);
+                }
+
+                bufferedReader.close();
+                inputStream.close();
+                Log.d("json", result.toString());
+                return new JSONObject(result.toString());
+            }
+            catch (MalformedURLException e)
+            {
+                e.printStackTrace();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(JSONObject jsonObject) {
+            GeoJsonLayer geoJsonLayer;
+            JSONArray rowJSONArray;
+            JSONArray countryJSONArray;
+            JSONObject countryJSONObject;
+            String countryName;
+
+            try
+            {
+                if (jsonObject != null)
+                {
+                    rowJSONArray = jsonObject.getJSONArray("rows");
+
+                    for (int i = 0; i < rowJSONArray.length(); i++) {
+                        countryJSONArray = rowJSONArray.getJSONArray(i);
+                        countryJSONObject = new JSONObject(countryJSONArray.getString(1));
+                        geoJsonLayer = new GeoJsonLayer(googleMap, countryJSONObject);
+                        geoJsonLayerArrayList.add(geoJsonLayer);
+                        geoJsonLayer.addLayerToMap();
+                    }
+                }
+            } catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
